2e0c52aaf84429e9de1a6f418c3362e403c582ff
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2024-02-01 14:37:01 +0300

    feat: add infinity scroll

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,9 +1,46 @@
 import React from 'react';
 import styles from './home.module.css';
 
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { useNavigate } from 'react-router-dom';
+
 function Home() {
+
+  const navigate = useNavigate();
+
+  const [state, setState] = React.useState({
+    items: Array.from({ length: 20 })
+  });
+
+  function fetchMoreData() {
+    setTimeout(() => {
+      setState({
+        items: state.items.concat(Array.from({ length: 20 }))
+      });
+    }, 300);
+  };
+
+  function handleOnClick(id: number) {
+    navigate('/topic/' + id);
+  }
+
   return (
-    <>Home</>
+    <>
+      <h1>Главная страница</h1>
+      <hr />
+      <InfiniteScroll
+        dataLength={state.items.length}
+        next={fetchMoreData}
+        hasMore={true}
+        loader={<h4>Загрузка...</h4>}
+      >
+        {state.items.map((i, index) => (
+          <div className={styles.card} key={index}>
+            №{index} - Заголовок - Текст - <button onClick={() => handleOnClick(index)}>Просмотр</button>
+          </div>
+        ))}
+      </InfiniteScroll>
+    </>
   );
 }
 
